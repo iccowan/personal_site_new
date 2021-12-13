8764ea024bbf83e4854f037a60839930072ebde5
Ian Cowan <ian@cowanemail.com>
2021-12-12 20:20:29 -0500

    Add github, linkedin, email links

---
+++ src/components/Links.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../css/Links.css";
+
+function Links() {
+  return (
+    <div>
+      <a data-testid="github-link" href="/external/https/github.com%2ficcowan/true"><i className="fab fa-github"></i></a>
+      <a data-testid="linkedin-link" href="/external/https/www.linkedin.com%2fin%2fian-cowan/true"><i className="fab fa-linkedin"></i></a>
+      <a data-testid="email-link" href="/external/mailto/ian@cowanemail.com/false"><i className="far fa-envelope"></i></a>
+    </div>
+  );
+}
+
+export default Links;
